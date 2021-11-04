9cb1a4c729a22ab86c6393e12375b0585e5af4aa
Read Langworthy <rlangworthy@gmail.com>
2021-11-04 13:53:32 -0700

    commit tree 2c7bb78d8a273844e0e048fef524bc2efa0b60e6

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+
+class Polygon {
+    constructor(array){
+        this.array = array;
+    }
+
+    get countSides(){
+        return( this.array.length )
+    }
+    get perimeter(){
+        return( this.array.reduce((previousValue, currentValue) => (previousValue + currentValue) ) )
+    }
+}
+
+class Triangle extends Polygon{
+    get isValid(){
+        return (this.array[0] + this.array[1] > this.array[2] &&  
+                this.array[0] + this.array[2] > this.array[1] &&  
+                this.array[1] + this.array[2] > this.array[0]  ) 
+    }
+}
+
+class Square extends Polygon{
+    get isValid(){
+        return(this.array[0] === this.array[1] &&
+               this.array[0] === this.array[2] &&
+               this.array[0] === this.array[3] )
+    }
+
+    get area(){
+        return(this.array[0] ** 2)
+    }
+}
